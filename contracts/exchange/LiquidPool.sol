36e4a589c2ab50636d1a5915caa2841004a4bfc1
ZlayaMorda <tarus01@mail.ru>
2022-08-05 16:48:03 +0300

    docstrings and review

--- contracts/exchange/LiquidPool.sol
+++ contracts/exchange/LiquidPool.sol
@@ -8,11 +8,22 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "./LiquidityToken.sol";
 import "./interfaces/ILiquidityToken.sol";
 
+/**
+Create LP token
+functions for stacke, withdraw, exchange tokens
+*/
 contract LiquidityPool is Ownable {
     address private token1;
     address private token2;
     address private liquidToken;
 
+    /**
+    @dev create LP token, set exchange tokens addresses
+    @param _token1 first token address
+    @param _token2 second token address
+    @param _fullName full name of the new LP token
+    @param _shortName short name of the new LP token
+    */
     constructor (
         address _token1, 
         address _token2,
@@ -30,6 +41,10 @@ contract LiquidityPool is Ownable {
         liquidToken = address(lpToken);
     }
 
+    /**
+    @dev the amount must be not null
+    @param _amount amount to check
+    */
     modifier getNullAmount(uint256 _amount) {
         require(
             _amount != 0,
@@ -38,6 +53,10 @@ contract LiquidityPool is Ownable {
         _;
     }
 
+    /**
+    @dev withdraw tokens from pull and burn LP tokens
+    @param _amountLP amount of the LP tokens
+    */
     function withdraw(uint256 _amountLP) external getNullAmount(_amountLP){
         IERC20 IToken1 = IERC20(token1);
         IERC20 IToken2 = IERC20(token2);
@@ -53,11 +72,13 @@ contract LiquidityPool is Ownable {
         uint256 sum_1 = (balance_1 / (balance_1 + balance_2)) * _amountLP;
         uint256 sum_2 = (balance_2 / (balance_2 + balance_1)) * _amountLP;
 
-        if(sum_1 == 0 && sum_2 == 0) {
-            revert("LiquidityPool:: sums try to withdraw are 0");
-        }
+        require(
+            sum_1 != 0 && sum_2 != 0,
+            "LiquidityPool:: sums try to withdraw are 0"
+        );
 
         ILiquidityToken(liquidToken).burn(_amountLP, msg.sender);
+
         if(sum_1 != 0) {
             require(
                 IToken1.transfer(msg.sender, sum_1),
@@ -73,9 +94,13 @@ contract LiquidityPool is Ownable {
 
     }
 
+    /**
+    @dev get token1, calculate num of token2 and stack they in pool
+    @param _token1 num of stacking tokens
+    */
     function transferThroughToken1(uint256 _token1) external getNullAmount(_token1){
-        uint256 balance_1 = nullToOne(IERC20(token1).balanceOf(address(this)));
-        uint256 balance_2 = nullToOne(IERC20(token2).balanceOf(address(this)));
+        uint256 balance_1 = _nullToOne(IERC20(token1).balanceOf(address(this)));
+        uint256 balance_2 = _nullToOne(IERC20(token2).balanceOf(address(this)));
 
         uint256 _token2 = _token1 * balance_2 / balance_1;
 
@@ -84,12 +109,16 @@ contract LiquidityPool is Ownable {
             "LiquidityPool:: too much little sum of invested tokens"
         );
 
-        transferToPull(_token1, _token2);
+        _transferToPull(_token1, _token2);
     }
 
+    /**
+    @dev get token2, calculate num of token1 and stack they in pool
+    @param _token2 num of stacking tokens
+    */
     function transferThroughToken2(uint256 _token2) external getNullAmount(_token2){
-        uint256 balance_1 = nullToOne(IERC20(token1).balanceOf(address(this)));
-        uint256 balance_2 = nullToOne(IERC20(token2).balanceOf(address(this)));
+        uint256 balance_1 = _nullToOne(IERC20(token1).balanceOf(address(this)));
+        uint256 balance_2 = _nullToOne(IERC20(token2).balanceOf(address(this)));
 
         uint256 _token1 = _token2 * balance_1 / balance_2;
 
@@ -98,22 +127,36 @@ contract LiquidityPool is Ownable {
             "LiquidityPool:: too much little sum of invested tokens"
         );
 
-        transferToPull(_token1, _token2);
+        _transferToPull(_token1, _token2);
     }
 
+    /**
+    @dev get balance of the tokens in pool
+    */
     function getPullBalances() external view returns(uint256, uint256) {
         return (IERC20(token1).balanceOf(address(this)), IERC20(token2).balanceOf(address(this)));
     }
 
+    /**
+    @dev get address of the LP token
+    */
     function getliquidToken() external view returns(address){
         return liquidToken;
     }
 
+    /**
+    @dev get addresses of tokens in the pool
+    */
     function getTokensPair() external view returns(address, address) {
         return (token1, token2);
     }
 
-    function transferToPull(uint256 _token1Sum, uint256 _token2Sum) private {
+    /**
+    @dev transfer tokens to pool
+    @param _token1Sum sum of the first token
+    @param _token2Sum sum of the second token
+    */
+    function _transferToPull(uint256 _token1Sum, uint256 _token2Sum) private {
         IERC20 IToken1 = IERC20(token1);
         IERC20 IToken2 = IERC20(token2);
         require(
@@ -133,9 +176,14 @@ contract LiquidityPool is Ownable {
         );
 
         ILiquidityToken(liquidToken).stacking(_token1Sum + _token2Sum, msg.sender);
-    }
+    } 
 
-    function nullToOne(uint256 _sum) private pure returns(uint256) {
+    /**
+    @dev check if sum null and change it to 1
+    @param _sum sum to check
+    @return 1 or _sum
+    */
+    function _nullToOne(uint256 _sum) private pure returns(uint256) {
         if(_sum == 0) {
             return 1;
         }
--- contracts/exchange/LiquidityToken.sol
+++ contracts/exchange/LiquidityToken.sol
@@ -4,30 +4,53 @@ pragma solidity ^0.8.12;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
+/**
+LP token for pools
+users get it instead of invested tokens in pool
+may burn it and get pool tokens back
+*/
 contract LiquidityToken is ERC20 {
-    address private pull;
+    address private pool;
 
+    /**
+    @dev set address of valid pool
+    @param _fullName full name of the new LP token
+    @param _shortName short name of the new LP token
+    */
     constructor(
         string memory _fullName, 
         string memory _shortName
         ) ERC20(_fullName, _shortName) {
 
-        pull = msg.sender;
+        pool = msg.sender;
     }
 
-    modifier pullCaller() {
+    /**
+    @dev Check if sender is valid pool
+    */
+    modifier poolCaller() {
         require(
-            msg.sender == pull,
-            "LiquidityToken:: not real pull"
+            msg.sender == pool,
+            "LiquidityToken:: not real pool"
         );
         _;
     }
 
-    function stacking(uint256 _amount, address _investor) external pullCaller{
+    /**
+    @dev minting new LP tokens for user
+    @param _amount num of the tokens
+    @param _investor user get tokens
+    */
+    function stacking(uint256 _amount, address _investor) external poolCaller{
         _mint(_investor, _amount);
     }
 
-    function burn(uint256 _amount, address _investor) external pullCaller{
+    /**
+    @dev burn user LP tokens
+    @param _amount num of the tokens
+    @param _investor user's address
+    */
+    function burn(uint256 _amount, address _investor) external poolCaller{
         _burn(_investor, _amount);
     }    
 }
